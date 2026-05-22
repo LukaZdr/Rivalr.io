c7ee54f592d28a38d7c91c5355a12ca14255d3df
Luka <luka.z@hotmail.de>
2026-05-22 20:24:20 +0200

    fix gray screen for prod

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/Rivalr.io/',
+  base: './',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
